32cbc1bcdf123bce9d5702043c8ab557ae240f90
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2019-03-05 17:19:05 +0200

    Refs #100860; Code cleanup

--- src/index.js
+++ src/index.js
@@ -14,25 +14,43 @@ const { JSDOM } = jsdom;
 
 const privacyscore_base = 'https://privacyscore.org/'
 
-const getData = async (item) => {
-    const { url } = item.url_settings;
+function getResults(file) {
+    const dom = new JSDOM(file);
+
+    const critical = dom.window.document.querySelectorAll(".col>.color-critical").length;
+    const bad = dom.window.document.querySelectorAll(".col>.color-bad").length;
+    const neutral = dom.window.document.querySelectorAll(".col>.color-neutral").length;
+    const good = dom.window.document.querySelectorAll(".col>.color-good").length;
+
+    var score = 0;
+    if (critical === 0){
+        const total = critical + bad + neutral + good;
+        score = good / total * 100;
+    }
+
+    var result = {};
+    if (!isNaN(score)){
+        result['privacyscore'] = score;
+    }
+
+    return result;
+}
+
+const triggerPrivacyScoreScan = async (url) => {
     return new Promise(async (resolve, reject) => {
         try {
-            const { reportDir } = item;
-            const reportFolder = garie_plugin.utils.helpers.reportDirNow(reportDir);
-
-            const scan_uri = privacyscore_base + "site/scan/";
-
-            const base_resp = await request({
+            //first go to https://privacyscore.org to get the xsrftoken and xsrfmiddlewaretoken
+            var response;
+            response = await request({
                 method: 'GET',
                 uri: privacyscore_base,
                 resolveWithFullResponse: true
             });
 
-            const dom = new JSDOM(base_resp.body);
+            const dom = new JSDOM(response.body);
             const csrfmiddlewaretoken = dom.window.document.querySelector("input[name='csrfmiddlewaretoken']").value;
 
-            const cookies = base_resp.headers['set-cookie'];
+            const cookies = response.headers['set-cookie'];
             var csrftoken = '';
             var regex = RegExp("\csrftoken=(.*?)\;");
             cookies.forEach(function(cookie){
@@ -41,13 +59,16 @@ const getData = async (item) => {
                 }
             });
 
+            //create the scan job on https://privacyscore.org
+            const scan_uri = privacyscore_base + "site/scan/";
+
             var headers = {
                 'Cookie': 'csrftoken='+csrftoken,
                 'Referer': 'https://privacyscore.org/',
                 'X-CSRFToken': csrftoken
             };
 
-            const response = await request({
+            response = await request({
                 method: 'POST',
                 uri: scan_uri,
                 form: {
@@ -59,73 +80,101 @@ const getData = async (item) => {
                 followAllRedirects: true
             });
 
+            //periodically get the status of the scan job
             const redirect_to = response.request.uri.path.split("/");
             const results_uri = privacyscore_base + "site/" + redirect_to[2];
 
-            var results_response;
+
             while(true){
-                results_response = await request({
+                response = await request({
                     method: 'GET',
                     uri: results_uri,
                     resolveWithFullResponse: true
                 });
-                if (!results_response.body.includes("SCAN IN PROGRESS")){
+                if (!response.body.includes("SCAN IN PROGRESS")){
                     break;
                 }
                 await sleep(500)
             }
+            resolve(results_uri);
+        } catch (err) {
+            console.log(`Failed to trigger the scan for ${url}`, err);
+            reject(`Failed to trigger the scan for ${url}`);
+        }
+    });
+}
+
+const getHtmlData = async (results_url, folder) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            // scrape the results url and save it to folder
             const options = {
-                urls: [{url: results_uri, filename: 'privacyscore.html'},],
-                directory: reportFolder,
+                urls: [{url: results_url, filename: 'privacyscore.html'},],
+                directory: folder
             };
-            const page_result = await scrape(options);
 
-            var html_data = page_result[0].text;
+            const html_result = await scrape(options);
 
+            var html_data = html_result[0].text;
+
+            //add extra style to hide the header/footer in our reports
             html_data = html_data.replace("</html>","<style type='text/css'>.navbar,.col-sm-4,.footer{display:none !important;} .col-md-4{display:block !important;}</style></html>");
 
-            const result_dom = new JSDOM(html_data);
-
-            const critical = result_dom.window.document.querySelectorAll(".col>.color-critical").length;
-            const bad = result_dom.window.document.querySelectorAll(".col>.color-bad").length;
-            const neutral = result_dom.window.document.querySelectorAll(".col>.color-neutral").length;
-            const good = result_dom.window.document.querySelectorAll(".col>.color-good").length;
-
-
-            var score = 0;
-            if (critical === 0){
-                const total = critical + bad + neutral + good;
-                score = good / total * 100;
-            }
-
-            var html_file = path.join(reportFolder, 'privacyscore.html');
+            var html_file = path.join(folder, 'privacyscore.html');
 
             fs.outputFile(html_file, html_data)
-            .then(() => console.log(`Saved privacyscore html file for ${url}`))
             .catch(err => {
-              console.log(err)
+              console.log(err);
             })
+            resolve (html_data);
+        } catch (err) {
+            console.log(`Failed to save html report`, err);
+            reject(`Failed to save html report`);
+        }
+    });
+}
 
-            const json_uri = results_uri + "/json/";
-            json_response = await request({
+const getJsonData = async (results_url, folder) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            // privacyscore.org only provides a highlighted json, so we have to transform in raw JSON
+            const json_url = results_url + "/json/";
+            const response = await request({
                 method: 'GET',
-                uri: json_uri,
+                uri: json_url,
                 resolveWithFullResponse: true
             });
-            const json_dom = new JSDOM(json_response.body);
-            const json_val = json_dom.window.document.querySelector(".highlight").textContent;
-            const json_data = JSON.parse(json_val);
+            const dom = new JSDOM(response.body);
+            const text_data = dom.window.document.querySelector(".highlight").textContent;
+            const json_data = JSON.parse(text_data);
 
-            var json_file = path.join(reportFolder, 'privacyscore.json');
+            var json_file = path.join(folder, 'privacyscore.json');
 
             fs.outputJson(json_file, json_data, {spaces: 2})
-            .then(() => console.log(`Saved privacyscore json file for ${url}`))
             .catch(err => {
-              console.log(err)
+              console.log(err);
             })
+            resolve ({});
+        } catch (err) {
+            console.log(`Failed to save json report`, err);
+            reject(`Failed to save json report`);
+        }
+    });
+}
 
-            var result = {}
-            result['privacyscore'] = score;
+const getData = async (item) => {
+    const { url } = item.url_settings;
+    return new Promise(async (resolve, reject) => {
+        try {
+            const { reportDir } = item;
+            const reportFolder = garie_plugin.utils.helpers.reportDirNow(reportDir);
+
+            const results_url = await triggerPrivacyScoreScan(url);
+
+            const html_data = await getHtmlData(results_url, reportFolder);
+            await getJsonData(results_url, reportFolder);
+
+            const result = getResults(html_data);
 
             resolve(result);
         } catch (err) {
